7e70a4dced0c0a9ad3ed08e684396a273dd6cec3
Saurav Chaudhary <sauravchaudhary717@gmail.com>
2024-07-12 14:47:18 +0530

    Aggregating & Ordering

--- Data & Models/book_store/book_outlet/views.py
+++ Data & Models/book_store/book_outlet/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-rating')
     return render(request, 'book_outlet/index.html', {
-        'books': books
+        'books': books,
+        'total_books': books.count(),
+        'average_rating': books.aggregate(Avg('rating'))
     })    
 
 def book_detail(request, slug):
