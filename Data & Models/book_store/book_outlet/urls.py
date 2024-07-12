006ff71bcda38781e0725aecc52de8452983a568
Saurav Chaudhary <sauravchaudhary717@gmail.com>
2024-07-12 14:09:14 +0530

    Working With URL Tag To Link Pages

--- Data & Models/book_store/book_outlet/urls.py
+++ Data & Models/book_store/book_outlet/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:book_id>/', views.book_detail, name='book_detail')
 ]
--- Data & Models/book_store/book_outlet/views.py
+++ Data & Models/book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Book
 
 # Create your views here.
@@ -7,3 +8,16 @@ def index(request):
     return render(request, 'book_outlet/index.html', {
         'books': books
     })    
+
+def book_detail(request, book_id):
+    # try:
+    #     book = Book.objects.get(id=book_id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, id=book_id)    
+    return render(request, 'book_outlet/book_detail.html', {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
